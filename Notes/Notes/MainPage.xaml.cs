30b7c7d853ff0ac1ae3950d8c6322d0e72c08c75
Tony <carlos.rr94@hotmail.com>
2020-06-10 00:34:44 -0600

    Agregar archivos de proyecto.

---
+++ Notes/Notes/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                editor1.Text = File.ReadAllText(_fileName);
+                editor2.Text = File.ReadAllText(_fileName);
+                editor3.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            //File.WriteAllText(_fileName, editor.Text);
+            int nun1 = Int16.Parse(editor1.Text);
+            int nun2 = Int16.Parse(editor2.Text);
+            int result = nun1 + nun2;
+            editor3.Text = result.ToString();
+        }
+
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            editor1.Text = string.Empty;
+            editor2.Text = string.Empty;
+            editor3.Text = string.Empty;
+        }
+    }
+}
